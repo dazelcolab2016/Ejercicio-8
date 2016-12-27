02a4c05ce5e6d25ac84cc1e136532afce70f0aab
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-27 15:15:18 -0500

    run diff

--- main.js
+++ main.js
@@ -1,38 +1,16 @@
-function monoB()
-{
-	var img = document.getElementById("img_mono_b");
-	var checkB = document.getElementById("mono_b");
-
-	if(checkB.checked)
-		img.innerHTML = '<img src="img/feliz.jpg">';
-	else
-		img.innerHTML = '<img src="img/triste.jpg">';
-}
-function monoA()
-{
-	var img = document.getElementById("img_mono_a");
-	var checkA = document.getElementById("mono_a");
-
-	if(checkA.checked)
-	{
-		img.innerHTML = '<img class="imagen" src="img/feliz.jpg">';
-	}else{
-		img.innerHTML = '<img src="img/triste.jpg">';
-	}
-}
-
 function evaluar()
 {
-	var aSmile = document.getElementById("mono_a").checked;
-	var bSmile = document.getElementById("mono_b").checked;
+	var numero = document.getElementById("numero").value;
 	var salida = document.getElementById("salida");
+    var diff   = 0;
 
-	if(aSmile == bSmile)
+	if(numero >= 21 )
 	{
-		//problemas
-		salida.innerHTML = "1";
+		diff = 2 * (numero - 21);
+		salida.innerHTML = diff;
 	}else{
-		//NO hay problema
-		salida.innerHTML = "0";
+		diff  = 21 - numero;
+        salida.innerHTML = diff;
 	}
 }
+
